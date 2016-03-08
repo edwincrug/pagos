3c22b43c4c6ca8274d9947623c645db6711f3f1a
edwincrug <edwincrug@gmail.com>
2016-03-08 11:18:11 -0600

    Confront

--- app/static/confront.js
+++ app/static/confront.js
@@ -1,5 +1,9 @@
 var global_settings = {
+<<<<<<< Updated upstream
 	//urlCORS: 'http://localhost/PagosApi',
 	urlCORS: 'http://localhost:56669',
+=======
+	urlCORS: 'http://192.168.20.9/PagosApi',
+>>>>>>> Stashed changes
 	liveReload: 1000
 } 	
