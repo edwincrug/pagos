dde60ca7fb5524b66aaba1764d33576d92bd5fd4
edwincrug <edwincrug@gmail.com>
2016-04-11 18:29:48 -0500

    Tamaño Link

--- app/static/AngularJS/Pago/pagoController.js
+++ app/static/AngularJS/Pago/pagoController.js
@@ -803,6 +803,7 @@ registrationModule.controller("pagoController", function ($scope, $http, $interv
 
 //LQMA congifura e inicializa el grid
 var ConfiguraGrid = function(){
+  
 $scope.idEmpleado = $rootScope.currentEmployee;
 $scope.gridOptions = {
         enableColumnResize: true,
@@ -845,7 +846,7 @@ $scope.gridOptions = {
          },
          { name: 'fechaPromesaPago', displayName: 'Fecha Promesa de Pago', type: 'date', cellFilter: 'date:"dd/MM/yyyy"', width: '17%'},
          { name: 'documento', displayName: '# Documento', width: '15%', enableCellEdit: false, headerTooltip: 'Documento # de factura del provedor', cellClass: 'cellToolTip' },
-         { name: 'ordenCompra', displayName: 'Orden de compra',width: '13%', enableCellEdit: false, cellTemplate:'<div class="urlTabla" ng-class="col.colIndex()" ><A class="urlTabla" HREF="http://192.168.20.9:3200/?id={{row.entity.ordenCompra}}&employee=' + $scope.idEmpleado + '" TARGET="_new">{{row.entity.ordenCompra}}</A></div>'},          
+         { name: 'ordenCompra', displayName: 'Orden de compra',width: '13%', enableCellEdit: false, cellTemplate:'<div class="urlTabla" ng-class="col.colIndex()" ><a tooltip="Ver en digitalización" class="urlTabla" href="http://192.168.20.9:3200/?id={{row.entity.ordenCompra}}&employee=' + $scope.idEmpleado + '" target="_new">{{row.entity.ordenCompra}}</a></div>'},          
          { name: 'monto', displayName: 'Monto', width: '15%', cellFilter: 'currency' , enableCellEdit: false},
          { name: 'saldo', displayName: 'Saldo', width: '15%', cellFilter: 'currency' , enableCellEdit: false},
          { name: 'tipo', width: '15%', displayName: 'Tipo', enableCellEdit: false },
